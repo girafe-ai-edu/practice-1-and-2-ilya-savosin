57e8cfb41b4ca29af4d6a7c5d65c6973893a4782
ilya-savosin <ilyasavosin605@gmail.com>
2024-09-19 01:19:55 +0300

    hw_1 done

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,8 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
+weight = input('Введите Ваш вес в кг: ')
+height = input('Введите Ваш рост в м (например, 1.73): ')
 
-
-#code here
+print(f"BIM = {float(weight) / float(height) ** 2:.1f} кг/м2")
 
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,9 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+string_of_number = input('Введите целое четырёхзначное число: ')
+
+digits = list(string_of_number)
+number = int(string_of_number)
+sum_of_digits = number % 10 + number // 10 % 10 + number // 100 % 10 + number // 1000
+print(" + ".join(digits), '=', sum_of_digits)
